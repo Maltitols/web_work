099b057e4456480dc846810c60361907eafc9b10
leechangho <maltitol7090@gmail.com>
2020-01-17 16:55:57 +0900

    파일목록에 페이지 추가

--- Step05_Login/WebContent/file/list.jsp
+++ Step05_Login/WebContent/file/list.jsp
@@ -4,9 +4,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%
-	//1. DB 에서 파일 목록을 얻어와서
-	List<FileDto> list=FileDao.getInstance().getList();
-	//2. 응답하기 
+	//세션에 있는 아이디 읽어오기 (로그인 하지 않았으면 null 이다)
+	String id=(String)session.getAttribute("id");
 %>    
 <!DOCTYPE html>
 <html>
@@ -16,6 +15,49 @@
 <jsp:include page="../include/resource.jsp"></jsp:include>
 </head>
 <body>
+<%
+	//한 페이지에 나타낼 row 의 갯수
+	final int PAGE_ROW_COUNT=5;
+	//하단 디스플레이 페이지 갯수
+	final int PAGE_DISPLAY_COUNT=3;
+	
+	//보여줄 페이지의 번호
+	int pageNum=1;
+	//보여줄 페이지의 번호가 파라미터로 전달되는지 읽어와 본다.	
+	String strPageNum=request.getParameter("pageNum");
+	if(strPageNum != null){//페이지 번호가 파라미터로 넘어온다면
+		//페이지 번호를 설정한다.
+		pageNum=Integer.parseInt(strPageNum);
+	}
+	//보여줄 페이지 데이터의 시작 ResultSet row 번호
+	int startRowNum=1+(pageNum-1)*PAGE_ROW_COUNT;
+	//보여줄 페이지 데이터의 끝 ResultSet row 번호
+	int endRowNum=pageNum*PAGE_ROW_COUNT;
+	
+	//전체 row 의 갯수를 읽어온다.
+	int totalRow=FileDao.getInstance().getCount();
+	//전체 페이지의 갯수 구하기
+	int totalPageCount=
+			(int)Math.ceil(totalRow/(double)PAGE_ROW_COUNT);
+	//시작 페이지 번호
+	int startPageNum=
+		1+((pageNum-1)/PAGE_DISPLAY_COUNT)*PAGE_DISPLAY_COUNT;
+	//끝 페이지 번호
+	int endPageNum=startPageNum+PAGE_DISPLAY_COUNT-1;
+	//끝 페이지 번호가 잘못된 값이라면 
+	if(totalPageCount < endPageNum){
+		endPageNum=totalPageCount; //보정해준다. 
+	}		
+	//DB 에서 파일 목록을 얻어와서
+	FileDto dto=new FileDto();
+	// CafeDto 객체에 위에서 계산된 startRowNum 과 endRowNum 을 담는다.
+	dto.setStartRowNum(startRowNum);
+	dto.setEndRowNum(endRowNum);
+
+	List<FileDto> list=FileDao.getInstance().getList(dto);
+	//응답하기 
+	
+%>
 <jsp:include page="../include/navbar.jsp">
 	<jsp:param value="file" name="category"/>
 </jsp:include>
@@ -47,11 +89,59 @@
 				<td><%=tmp.getFileSize() %></td>
 				<td><%=tmp.getDownCount() %></td>
 				<td><%=tmp.getRegdate() %></td>
+				<td>
+					<%if(tmp.getWriter().equals(id)){ %>
+						<a href="javascript:deleteConfirm(<%=tmp.getNum()%>)">삭제</a>
+					<%} %>
+				</td>
 			</tr>
 		<%} %>	
 		</tbody>
 	</table>
-	<a href="${pageContext.request.contextPath }/file/private/upload_form.jsp">업로드 하러 가기</a>
+	<a href="${pageContext.request.contextPath }/file/private/upload_form.jsp">파일 업로드</a>
+	
+	<div class="page-display">
+		<ul class="pagination pagination-sm">
+			<%if(startPageNum != 1){ %>
+				<li>
+					<a href="list.jsp?pageNum=<%=startPageNum-1 %>">&laquo;</a>
+				</li>
+			<%}else{ %>
+				<li class="disabled">
+					<a href="javascript:">&laquo;</a>
+				</li>
+			<%} %>
+			<%for(int i=startPageNum; i<=endPageNum; i++){ %>
+				<%if(i == pageNum){ %>
+					<li class="active">
+						<a href="list.jsp?pageNum=<%=i %>"><%=i %></a>
+					</li>
+				<%}else{ %>
+					<li>
+						<a href="list.jsp?pageNum=<%=i %>"><%=i %></a>
+					</li>
+				<%} %>
+			<%} %>
+			<%if(endPageNum < totalPageCount){ %>
+				<li>
+					<a href="list.jsp?pageNum=<%=endPageNum+1 %>">&raquo;</a>
+				</li>
+			<%}else{ %>
+				<li class="disabled">
+					<a href="javascript:">&raquo;</a>
+				</li>
+			<%} %>
+		</ul>
+	</div>
 </div>
+<script>
+	//삭제 여부를 확인하고 삭제를 진행하는 javascript 함수
+	function deleteConfirm(num){
+		var isDelete=confirm(num+" 번 파일을 삭제 하시겠습니까?");
+		if(isDelete){
+			location.href="private/delete.jsp?num="+num;
+		}
+	}
+</script>
 </body>
 </html>
--- Step05_Login/src/test/file/dao/FileDao.java
+++ Step05_Login/src/test/file/dao/FileDao.java
@@ -18,16 +18,48 @@ public class FileDao {
 		}
 		return dao;
 	}
-	//파일 다운로드 횟수를 증가시키는 메소드
-	public boolean addDownCount(int num) {
+	//글 전체의 갯수를 리턴하는 메소드
+		public int getCount() {
+			int rowCount=0;
+			Connection conn = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			try {
+				conn = new DbcpBean().getConn();
+				String sql = "SELECT MAX(ROWNUM) AS count"
+						+ " FROM board_file";
+				pstmt = conn.prepareStatement(sql);
+				// ? 에 값 바인딩 
+				rs = pstmt.executeQuery();
+				while (rs.next()) {
+					rowCount=rs.getInt("count");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					if (rs != null)
+						rs.close();
+					if (pstmt != null)
+						pstmt.close();
+					//connection pool 에 반납하기 
+					if (conn != null)
+						conn.close();
+				} catch (Exception e) {
+				}
+			}
+			return rowCount;
+		}
+		
+	//파일 정보를 삭제하는 메소드
+	public boolean delete(int num) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		int flag = 0;
 		try {
 			conn = new DbcpBean().getConn();
-			String sql = "update board_file"
-					+ " set downCount=downCount+1"
-					+ " where num=?";
+			String sql = "DELETE FROM board_file"
+					+ " WHERE num=?";
 			pstmt = conn.prepareStatement(sql);
 			// ? 에 값 바인딩 하기
 			pstmt.setInt(1, num);
@@ -50,7 +82,39 @@ public class FileDao {
 		}
 	}
 	
-	//인자로 전달되는 번호에 해당하는 파일 정보를 리턴해주는 메소드
+	//파일 다운로드 횟수를 증가 시키는 메소드
+	public boolean addDownCount(int num) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int flag = 0;
+		try {
+			conn = new DbcpBean().getConn();
+			String sql = "UPDATE board_file"
+					+ " SET downCount=downCount+1"
+					+ " WHERE num=?";
+			pstmt = conn.prepareStatement(sql);
+			// ? 에 값 바인딩 하기
+			pstmt.setInt(1, num);
+			flag = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		if (flag > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	//인자로 전달되는 파일번호에 해당하는 파일 정보를 리턴해주는 메소드
 	public FileDto getData(int num) {
 		FileDto dto=null;
 		
@@ -59,10 +123,10 @@ public class FileDao {
 		ResultSet rs = null;
 		try {
 			conn = new DbcpBean().getConn();
-			String sql = "select writer, title, orgFileName, "
-					+ " saveFileName, fileSize, downCount, regdate"
-					+ " from board_file"
-					+ " where num=?";
+			String sql = "SELECT writer,title,saveFileName,"
+					+ " orgFileName,fileSize,downCount,regdate"
+					+ " FROM board_file"
+					+ " WHERE num=?";
 			pstmt = conn.prepareStatement(sql);
 			// ? 에 값 바인딩 
 			pstmt.setInt(1, num);
@@ -76,7 +140,7 @@ public class FileDao {
 				dto.setSaveFileName(rs.getString("saveFileName"));
 				dto.setFileSize(rs.getLong("fileSize"));
 				dto.setDownCount(rs.getInt("downCount"));
-				dto.setRegdate(rs.getString("regdate"));				
+				dto.setRegdate(rs.getString("regdate"));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -96,24 +160,24 @@ public class FileDao {
 		return dto;
 	}
 	
-	//업로드된 파일의 정보를 DB에 저장하는 메소드
+	//업로드된 파일의 정보를 DB 에 저장하는 메소드
 	public boolean insert(FileDto dto) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		int flag = 0;
 		try {
 			conn = new DbcpBean().getConn();
-			String sql = "insert into board_file"
-					+ " (num, writer, title, orgFileName, saveFileName,"
-					+ " fileSize, regdate)"
-					+ " VALUES(board_file_seq.nextval,?,?,?,?,?,sysdate)";
+			String sql = "INSERT INTO board_file"
+					+ " (num,writer,title,orgFileName,saveFileName,"
+					+ " fileSize,regdate)"
+					+ " VALUES(board_file_seq.NEXTVAL,?,?,?,?,?,SYSDATE)";
 			pstmt = conn.prepareStatement(sql);
 			// ? 에 값 바인딩 하기
 			pstmt.setString(1, dto.getWriter());
 			pstmt.setString(2, dto.getTitle());
 			pstmt.setString(3, dto.getOrgFileName());
 			pstmt.setString(4, dto.getSaveFileName());
-			pstmt.setLong(5, dto.getFileSize());		
+			pstmt.setLong(5, dto.getFileSize());
 			flag = pstmt.executeUpdate();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -134,31 +198,37 @@ public class FileDao {
 	}
 	
 	//파일 목록을 리턴하는 메소드
-	public List<FileDto> getList(){
+	public List<FileDto> getList(FileDto dto){
 		List<FileDto> list=new ArrayList<>();
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		try {
 			conn = new DbcpBean().getConn();
-			String sql = "select num, writer, title, orgFileName,"
-					+ "fileSize, downCount, regdate"
-					+ " from board_file"
-					+ " order by num desc";
-			pstmt = conn.prepareStatement(sql);
+			String sql="SELECT *"
+					+ " FROM"
+					+ " (SELECT result1.*, ROWNUM AS rnum"
+					+ " FROM"
+					+ " (SELECT num,writer,title,orgFileName,saveFileName,"
+					+ " fileSize,downCount,regdate"
+					+ " FROM board_file"
+					+ " ORDER BY num DESC) result1)"
+					+ " WHERE rnum BETWEEN ? AND ?";
+			pstmt=conn.prepareStatement(sql);
 			// ? 에 값 바인딩 
-
+			pstmt.setInt(1, dto.getStartRowNum());
+			pstmt.setInt(2, dto.getEndRowNum());
 			rs = pstmt.executeQuery();
 			while (rs.next()) {
-				FileDto dto=new FileDto();
-				dto.setNum(rs.getInt("num"));
-				dto.setWriter(rs.getString("writer"));
-				dto.setTitle(rs.getString("title"));
-				dto.setOrgFileName(rs.getString("orgFileName"));
-				dto.setFileSize(rs.getLong("fileSize"));
-				dto.setDownCount(rs.getInt("downCount"));
-				dto.setRegdate(rs.getString("regdate"));
-				list.add(dto);
+				FileDto tmp=new FileDto();
+				tmp.setNum(rs.getInt("num"));
+				tmp.setWriter(rs.getString("writer"));
+				tmp.setTitle(rs.getString("title"));
+				tmp.setOrgFileName(rs.getString("orgFileName"));
+				tmp.setFileSize(rs.getLong("fileSize"));
+				tmp.setDownCount(rs.getInt("downCount"));
+				tmp.setRegdate(rs.getString("regdate"));
+				list.add(tmp);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
