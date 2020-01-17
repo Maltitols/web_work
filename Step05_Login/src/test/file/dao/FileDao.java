722faa6e1e71c4a664b80fe7bcd223d8a3d950a9
leechangho <maltitol7090@gmail.com>
2020-01-17 15:09:04 +0900

    파일 다운로드 완성

--- Step05_Login/src/test/file/dao/FileDao.java
+++ Step05_Login/src/test/file/dao/FileDao.java
@@ -1,5 +1,14 @@
 package test.file.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import test.file.dto.FileDto;
+import test.util.DbcpBean;
+
 public class FileDao {
 	private static FileDao dao;
 	private FileDao() {}
@@ -9,5 +18,162 @@ public class FileDao {
 		}
 		return dao;
 	}
+	//파일 다운로드 횟수를 증가시키는 메소드
+	public boolean addDownCount(int num) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int flag = 0;
+		try {
+			conn = new DbcpBean().getConn();
+			String sql = "update board_file"
+					+ " set downCount=downCount+1"
+					+ " where num=?";
+			pstmt = conn.prepareStatement(sql);
+			// ? 에 값 바인딩 하기
+			pstmt.setInt(1, num);
+			flag = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		if (flag > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 	
+	//인자로 전달되는 번호에 해당하는 파일 정보를 리턴해주는 메소드
+	public FileDto getData(int num) {
+		FileDto dto=null;
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();
+			String sql = "select writer, title, orgFileName, "
+					+ " saveFileName, fileSize, downCount, regdate"
+					+ " from board_file"
+					+ " where num=?";
+			pstmt = conn.prepareStatement(sql);
+			// ? 에 값 바인딩 
+			pstmt.setInt(1, num);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				dto=new FileDto();
+				dto.setNum(num);
+				dto.setWriter(rs.getString("writer"));
+				dto.setTitle(rs.getString("title"));
+				dto.setOrgFileName(rs.getString("orgFileName"));
+				dto.setSaveFileName(rs.getString("saveFileName"));
+				dto.setFileSize(rs.getLong("fileSize"));
+				dto.setDownCount(rs.getInt("downCount"));
+				dto.setRegdate(rs.getString("regdate"));				
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				//connection pool 에 반납하기 
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		
+		return dto;
+	}
+	
+	//업로드된 파일의 정보를 DB에 저장하는 메소드
+	public boolean insert(FileDto dto) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int flag = 0;
+		try {
+			conn = new DbcpBean().getConn();
+			String sql = "insert into board_file"
+					+ " (num, writer, title, orgFileName, saveFileName,"
+					+ " fileSize, regdate)"
+					+ " VALUES(board_file_seq.nextval,?,?,?,?,?,sysdate)";
+			pstmt = conn.prepareStatement(sql);
+			// ? 에 값 바인딩 하기
+			pstmt.setString(1, dto.getWriter());
+			pstmt.setString(2, dto.getTitle());
+			pstmt.setString(3, dto.getOrgFileName());
+			pstmt.setString(4, dto.getSaveFileName());
+			pstmt.setLong(5, dto.getFileSize());		
+			flag = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		if (flag > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	//파일 목록을 리턴하는 메소드
+	public List<FileDto> getList(){
+		List<FileDto> list=new ArrayList<>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();
+			String sql = "select num, writer, title, orgFileName,"
+					+ "fileSize, downCount, regdate"
+					+ " from board_file"
+					+ " order by num desc";
+			pstmt = conn.prepareStatement(sql);
+			// ? 에 값 바인딩 
+
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				FileDto dto=new FileDto();
+				dto.setNum(rs.getInt("num"));
+				dto.setWriter(rs.getString("writer"));
+				dto.setTitle(rs.getString("title"));
+				dto.setOrgFileName(rs.getString("orgFileName"));
+				dto.setFileSize(rs.getLong("fileSize"));
+				dto.setDownCount(rs.getInt("downCount"));
+				dto.setRegdate(rs.getString("regdate"));
+				list.add(dto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				//connection pool 에 반납하기 
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return list;
+	}
 }
