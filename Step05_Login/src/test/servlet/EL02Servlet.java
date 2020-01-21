c99bc17cd76005b3d05a5a0eee3da097251a2dd2
leechangho <maltitol7090@gmail.com>
2020-01-21 11:00:54 +0900

    EL 테스트예제 2

---
+++ Step05_Login/src/test/servlet/EL02Servlet.java
@@ -0,0 +1,27 @@
+package test.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import test.users.dto.UsersDto;
+
+@WebServlet("/el02")
+public class EL02Servlet extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		UsersDto dto=new UsersDto();
+		dto.setId("kimkura");
+		dto.setEmail("aaa@naver.com");
+		//request 영역에 dto 담기
+		req.setAttribute("dto", dto);
+		//jsp페이지로 포워드
+		RequestDispatcher rd=req.getRequestDispatcher("/el/test02.jsp");
+		rd.forward(req, resp);
+	}
+}
